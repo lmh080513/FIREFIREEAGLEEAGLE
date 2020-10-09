66624b8bfa2d53f68e93e7e6f26c5b8e87a2e08c
strike-0513 <72585458+strike-0513@users.noreply.github.com>
2020-10-09 14:31:35 +0900

    Update .py

--- .py
+++ .py
@@ -13,8 +13,8 @@ async def on_message(message):
     if message.content.startswitch("FIRE EAGLE소개"):
         await message.chennal.send("FIRE EAGLE은 GTA5 ONLINE의 에어쇼이며, 항공팀/ 지상팀/ 홍보팀으로 나누어져 있습니다.")
 
-
-client.run("NzYzOTY1NDM3Mjk3MDMzMjI2.X3_YQg.lYSvcd_oasgFt1ECueQh2SIGcI8")
+access_token = os.environ ["BOT_TOKEN"]
+client.run("access_token")
 
 
 
